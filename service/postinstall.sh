a6bfb425b9aa6cb63d25f57102372a4918289406
René Jochum <jochumdev@gmail.com>
2025-01-20 14:20:26 +0100

    fix: postinstall script

--- service/postinstall.sh
+++ service/postinstall.sh
@@ -33,7 +33,6 @@ cleanInstall() {
 upgrade() {
     printf "\033[32m Post Install of an upgrade\033[0m\n"
     # Step 3(upgrade), do what you need
-    ...
 }
 
 # Step 2, check if this is a clean install or an upgrade
